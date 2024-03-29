e8b286d3312314bb644ed2b2e151899cddbe913a
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2024-03-29 11:25:45 +0200

    [dev] explains fnaf lore

--- src/MemeIndex-Core/Controllers/IndexingController.cs
+++ src/MemeIndex-Core/Controllers/IndexingController.cs
@@ -1,5 +1,7 @@
+using MemeIndex_Core.Data;
 using MemeIndex_Core.Services;
 using MemeIndex_Core.Utils;
+using Microsoft.EntityFrameworkCore;
 
 namespace MemeIndex_Core.Controllers;
 
@@ -8,12 +10,20 @@ public class IndexingController
     private readonly FileWatchService _watch;
     private readonly IFileService _fileService;
     private readonly IDirectoryService _directoryService;
+    private readonly MemeDbContext _context;
 
-    public IndexingController(FileWatchService watch, IFileService fileService, IDirectoryService directoryService)
+    public IndexingController
+    (
+        FileWatchService watch,
+        IFileService fileService,
+        IDirectoryService directoryService,
+        MemeDbContext context
+    )
     {
         _watch = watch;
         _fileService = fileService;
         _directoryService = directoryService;
+        _context = context;
     }
 
     public async Task AddDirectory(string path)
@@ -27,11 +37,7 @@ public class IndexingController
             Logger.Log(ConsoleColor.Magenta, "Directory [{0}] added", path);
 
             // add to db all files
-            var directory = new DirectoryInfo(path);
-            var files = Helpers.GetImageExtensions()
-                .Select(x => directory.GetFiles($"*{x}", SearchOption.AllDirectories))
-                .SelectMany(x => x)
-                .ToList();
+            var files = GetImageFiles(path);
 
             Logger.Log(ConsoleColor.Magenta, "Files: {0}", files.Count);
 
@@ -40,6 +46,14 @@ public class IndexingController
 
             Logger.Log("Done", ConsoleColor.Magenta);
         }
+    }
+
+    private static List<FileInfo> GetImageFiles(string path)
+    {
+        var directory = new DirectoryInfo(path);
+        return Helpers.GetImageExtensions()
+            .SelectMany(x => directory.GetFiles($"*{x}", SearchOption.AllDirectories))
+            .ToList();
     }
 
     /// <summary>
@@ -82,7 +96,62 @@ public class IndexingController
         // 1. a bunch of new files added to db
         //    [select files left join text]
         // 2. these files are slowly processed [by ocr ang color-tag] in the background
-        
+
+        var directories = _directoryService.GetTracked().Where(x => Directory.Exists(x.Path)).ToList();
+        var directoriesByPath = directories.ToDictionary(x => x.Path);
+
+        // get existing files > filter missing in db
+
+        var files = directories.SelectMany(x => GetImageFiles(x.Path));
+
+        // files present in fs, but missing in db
+        var unknownDirectoryFiles = new List<FileInfo>();
+        var unknownFiles = new List<FileInfo>();
+
+        foreach (var fileInfo in files)
+        {
+            var unknownDirectory = directoriesByPath.TryGetValue(fileInfo.DirectoryName!, out var directory) == false;
+            if (unknownDirectory)
+            {
+                unknownDirectoryFiles.Add(fileInfo); // new directory (created / moved / renamed)
+                continue;
+            }
+
+            var fileEntity = _context.Files.FirstOrDefault
+            (
+                file => file.DirectoryId == directory!.Id && file.Name == fileInfo.Name
+            );
+            if (fileEntity is null)
+            {
+                unknownFiles.Add(fileInfo); // new file (created / moved / renamed)
+            }
+
+            /*if (FileWasUpdated(fileInfo, fileEntity)) <-- these will be selected from db later, on processing phase
+            {
+                changed.Add(fileInfo); // file was edited (needs reprocessing)
+            }*/
+        }
+
+        // files present in db, but missing in fs
+        var missingFiles = _context.Files
+            .Include(x => x.Directory)
+            .Where(x => directories.Select(dir => dir.Id).Contains(x.DirectoryId))
+            .Where(x => !File.Exists(Path.Combine(x.Directory.Path, x.Name)))
+            .ToList();
+
+        // try to match unknown files to missing ones
+        //  unknownDirectoryFiles > check db on name ^ size ^ modified > 1 (renaming doesn't update modified date)
+        //      Y: create new db-directory for these files, update files by id
+        //      N: add to unknownFiles
+        //  unknownFiles > check db on name ^ size ^ modified > 1
+        //      Y: update files by id
+        //      N: add to db as new
+
+        // add unmatched
+
+        // remove empty directories from db
+
+
         // WHEN NEW FILE(s) ADDED (spotted by file watcher)
         // 1. file change object added to purgatory
         // 2. after 0.5 second changes are interpreted and db is updated
@@ -93,6 +162,25 @@ public class IndexingController
         // IF FILE ADDED:
         // 3. file added to db
         // 4. file processed in the background
+    }
+
+    private bool FilesAreTheSame(FileInfo fileInfo, Entities.File entity)
+    {
+        var sum = 0;
+
+        if (fileInfo.Name   == entity.Name) sum++;
+        if (fileInfo.Length == entity.Size) sum++;
+        if (fileInfo.LastWriteTimeUtc == entity.Modified) sum++;
+        if (fileInfo. CreationTimeUtc == entity. Created) sum++;
+
+        return sum > 1;
+    }
+
+    private bool FileWasUpdated(FileInfo fileInfo, Entities.File entity)
+    {
+        return fileInfo.Length != entity.Size ||
+               fileInfo.LastWriteTimeUtc > entity.Tracked ||
+               fileInfo.CreationTimeUtc  > entity.Tracked;
     }
 
     public void StartIndexing()
