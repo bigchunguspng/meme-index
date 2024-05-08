d12dede1e55488529e332f3dd08cc93ce0b31b67
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2024-05-08 19:51:15 +0300

    Wrapped color tag creation in try-catch

--- src/MemeIndex-Core/Services/ImageToText/ColorTag/ColorTagService.cs
+++ src/MemeIndex-Core/Services/ImageToText/ColorTag/ColorTagService.cs
@@ -41,86 +41,94 @@ public class ColorTagService : IImageToTextService
 
     public async Task<List<RankedWord>?> GetTextRepresentation(string path)
     {
-        // CHECK FILE
-        var file = new FileInfo(path);
-        if (file.Exists == false)
+        try
         {
-            return null;
-        }
-
-        // GET DATA / MEASUREMENTS
-        /*var info = await ImageHelpers.GetImageInfo(file.FullName);
-        var isRgba = info.PixelType.BitsPerPixel == 32;*/
-
-        //using var image = isRgba ? Image.Load<Rgba32>(file.FullName) : Image.Load<Rgb24>(file.FullName);
-        using var image = await Image.LoadAsync<Rgba32>(file.FullName);
-
-        var w = image.Width;
-        var h = image.Height;
-
-        var margin = Math.Min(w, h) < 80 ? 1 : 2;
-
-        var w2 = w - 2 * margin;
-        var h2 = h - 2 * margin;
-
-        var stepX = GetStep(w2);
-        var stepY = GetStep(h2);
-
-        int GetStep(int side) =>
-            side < 80
-                ? Math.Max(side >> 3, 4)
-                : side < 720
-                    ? side >> 4
-                    : side >> 5;
-
-        var chunksX = w2 / stepX;
-        var chunksY = h2 / stepY;
-
-        var dots = new List<KeyValuePair<string, Rgba32>>();
-
-        // SCAN IMAGE
-        for (var x = (w - stepX * (chunksX - 1)) / 2; x < w - margin; x += stepX)
-        for (var y = (h - stepY * (chunksY - 1)) / 2; y < h - margin; y += stepY)
-        {
-            var colors = new Rgba32[4];
-            var index = 0;
-            for (var i = -margin; i <= margin; i += 2 * margin)
-            for (var j = -margin; j <= margin; j += 2 * margin)
+            // CHECK FILE
+            var file = new FileInfo(path);
+            if (file.Exists == false)
             {
-                colors[index++] = image[x + i, y + j];
-#if DEBUG
-                image[x + i, y + j] = Color.Red;
-#endif
+                return null;
             }
 
-            var color = FindClosestKnownColor(ColorHelpers.GetAverageColor(colors));
-            dots.Add(color);
-        }
+            // GET DATA / MEASUREMENTS
+            /*var info = await ImageHelpers.GetImageInfo(file.FullName);
+            var isRgba = info.PixelType.BitsPerPixel == 32;*/
+
+            //using var image = isRgba ? Image.Load<Rgba32>(file.FullName) : Image.Load<Rgb24>(file.FullName);
+            using var image = await Image.LoadAsync<Rgba32>(file.FullName);
+
+            var w = image.Width;
+            var h = image.Height;
+
+            var margin = Math.Min(w, h) < 80 ? 1 : 2;
+
+            var w2 = w - 2 * margin;
+            var h2 = h - 2 * margin;
+
+            var stepX = GetStep(w2);
+            var stepY = GetStep(h2);
+
+            int GetStep(int side) =>
+                side < 80
+                    ? Math.Max(side >> 3, 4)
+                    : side < 720
+                        ? side >> 4
+                        : side >> 5;
+
+            var chunksX = w2 / stepX;
+            var chunksY = h2 / stepY;
+
+            var dots = new List<KeyValuePair<string, Rgba32>>();
+
+            // SCAN IMAGE
+            for (var x = (w - stepX * (chunksX - 1)) / 2; x < w - margin; x += stepX)
+            for (var y = (h - stepY * (chunksY - 1)) / 2; y < h - margin; y += stepY)
+            {
+                var colors = new Rgba32[4];
+                var index = 0;
+                for (var i = -margin; i <= margin; i += 2 * margin)
+                for (var j = -margin; j <= margin; j += 2 * margin)
+                {
+                    colors[index++] = image[x + i, y + j];
+#if DEBUG
+                    image[x + i, y + j] = Color.Red;
+#endif
+                }
+
+                var color = FindClosestKnownColor(ColorHelpers.GetAverageColor(colors));
+                dots.Add(color);
+            }
 
 #if DEBUG
-        var ticks = DateTime.UtcNow.Ticks;
-        var name = $"baka-{Math.Abs(path.GetHashCode())}-{ticks >> 32}";
-        Directory.CreateDirectory("img");
-        await image.SaveAsJpegAsync(Path.Combine("img", $"{name}-dots.jpg"), _defaultJpegEncoder);
+            var ticks = DateTime.UtcNow.Ticks;
+            var name = $"baka-{Math.Abs(path.GetHashCode())}-{ticks >> 32}";
+            Directory.CreateDirectory("img");
+            await image.SaveAsJpegAsync(Path.Combine("img", $"{name}-dots.jpg"), _defaultJpegEncoder);
 
-        for (var x = 0; x < w; x++)
-        for (var y = 0; y < h; y++)
-        {
-            var dot = x / stepX * chunksY + y / stepY;
-            image[x, y] = dots[Math.Clamp(dot, 0, dots.Count - 1)].Value;
-        }
+            for (var x = 0; x < w; x++)
+            for (var y = 0; y < h; y++)
+            {
+                var dot = x / stepX * chunksY + y / stepY;
+                image[x, y] = dots[Math.Clamp(dot, 0, dots.Count - 1)].Value;
+            }
 
-        await image.SaveAsJpegAsync(Path.Combine("img", $"{name}-colors.jpg"), _defaultJpegEncoder);
+            await image.SaveAsJpegAsync(Path.Combine("img", $"{name}-colors.jpg"), _defaultJpegEncoder);
 #endif
 
-        var data = dots
-            .Select(x => x.Key)
-            .GroupBy(x => x)
-            .OrderByDescending(g => g.Count())
-            .Select((x, i) => new RankedWord(x.Key, i))
-            .ToList();
+            var data = dots
+                .Select(x => x.Key)
+                .GroupBy(x => x)
+                .OrderByDescending(g => g.Count())
+                .Select((x, i) => new RankedWord(x.Key, i))
+                .ToList();
 
-        return data;
+            return data;
+        }
+        catch (Exception e)
+        {
+            Logger.LogError(nameof(ColorTagService), e);
+            return null;
+        }
     }
 
     private KeyValuePair<string, Rgba32> FindClosestKnownColor(Rgba32 color)
