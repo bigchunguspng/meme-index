cf6390d968d21f830efa7938d47b8c8edd39fc93
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2025-11-23 18:35:52 +0200

    +more tools

---
+++ src/MemeIndex/Tools/Backrooms/Extensions/Extensions_Generic.cs
@@ -0,0 +1,130 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace MemeIndex.Tools.Backrooms.Extensions;
+
+public static class Extensions_Generic
+{
+    public static T Fluent<T>
+        (this T obj, Action action)
+    {
+        action();
+        return obj;
+    }
+
+    public static bool TryGetValue_Failed<TKey,TValue>
+    (
+        this IDictionary<TKey, TValue> dictionary,
+        TKey key,
+        [MaybeNullWhen(true)] out TValue value
+    ) where TKey : notnull
+    {
+        return dictionary.TryGetValue(key, out value).Failed();
+    }
+
+    public static Span<T> Slice<T>(this T[] array, int length)
+    {
+        return array.AsSpan(0, length);
+    }
+
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (var element in source) action(element);
+    }
+
+    public static string? ToLower<T>(this T obj)
+    {
+        return obj?.ToString()?.ToLower();
+    }
+
+    public static T GetRandomMemeber<T>() where T : struct, Enum
+    {
+        var values = Enum.GetValues<T>();
+        return (T)values.GetValue(Random.Shared.Next(values.Length))!;
+    }
+
+    public static T PickAny<T>(this ICollection<T> collection)
+    {
+        return collection.ElementAt(Random.Shared.Next(collection.Count));
+    }
+
+    public static int FindIndex<T>(this T[] array, Predicate<T> match)
+    {
+        var l = array.Length;
+        for (var i = 0; i < l; i++)
+        {
+            if (match(array[i])) return i;
+        }
+
+        return -1;
+    }
+
+    public static int FindLastIndex<T>(this T[] array, Predicate<T> match)
+    {
+        for (var i = array.Length - 1; i >= 0; i--)
+        {
+            if (match(array[i])) return i;
+        }
+
+        return -1;
+    }
+
+    public static async Task<T?> OrDefault_OnException<T>(this Task<T> getThing)
+    {
+        try
+        {
+            return await getThing;
+        }
+        catch
+        {
+            return default;
+        }
+    }
+
+    /// Creates an array of given length, populated by given factory.
+    public static T[] Times<T>(this int length, Func<T> factory)
+    {
+        var array = new T[length];
+        for (var i = 0; i < length; i++)
+            array[i] = factory();
+        return array;
+    }
+
+    /// Creates an array of given length, populated by given factory.
+    /// Factory accepts item index.
+    public static T[] Times<T>(this int length, Func<int, T> factory)
+    {
+        var array = new T[length];
+        for (var i = 0; i < length; i++)
+            array[i] = factory(i);
+        return array;
+    }
+
+    // Meme-Index
+
+    /// Executes an action based on a condition value.
+    public static void Execute(this bool condition, Action onTrue, Action onFalse)
+    {
+        (condition ? onTrue : onFalse)();
+    }
+
+    /// Just a wrapper for a ternary operator (useful with delegates). 
+    public static T Switch<T>(this bool condition, T onTrue, T onFalse)
+    {
+        return condition ? onTrue : onFalse;
+    }
+
+    public static void ForEachTry<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (var item in source)
+        {
+            try
+            {
+                action(item);
+            }
+            catch
+            {
+                /* xd */
+            }
+        }
+    }
+}
---
+++ src/MemeIndex/Tools/Backrooms/Helpers/Desert.cs
@@ -0,0 +1,71 @@
+using System.Security.Cryptography;
+
+namespace MemeIndex.Tools.Backrooms.Helpers;
+
+/// Random string generator.
+public static class Desert
+{
+    private const string
+        _chars32 =
+            "0123456789"
+          + "ABCDEFGHJKLNPQRSTUVXYZ", // IMOW out
+        _chars64 =
+            "0123456789"
+          + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+          + "abcdefghijklmnopqrstuvwxyz_-";
+
+    /// Get a random char sequence of given length. <br/>
+    /// Style: numbers, uppercase. <br/>
+    /// Number of possible values = 32 ^ length.
+    public static string GetSand
+        (int length = 4) => GetSoil(length, 0b00011111, _chars32);
+
+    /// Get a random char sequence of given length. <br/>
+    /// Style: numbers, uppercase, lowercase, _, -. <br/>
+    /// Number of possible values = 64 ^ length.
+    public static string GetSilt
+        (int length = 4) => GetSoil(length, 0b00111111, _chars64);
+
+    private static string GetSoil(int length, byte mask, string alphabet)
+    {
+        Span<byte> bytes = stackalloc byte[length];
+        RandomNumberGenerator.Fill(bytes);
+        return TurnIntoSoil(bytes, mask, alphabet);
+    }
+
+    public static string TurnIntoSand
+        (Span<byte> bytes) => TurnIntoSoil(bytes, 0b00011111, _chars32);
+    public static string TurnIntoSilt
+        (Span<byte> bytes) => TurnIntoSoil(bytes, 0b00111111, _chars64);
+
+    private static string TurnIntoSoil(Span<byte> bytes, byte mask, string alphabet)
+    {
+        var length = bytes.Length;
+        Span<char> chars = stackalloc char[length];
+
+        for (var i = 0; i < length; i++)
+        {
+            var index = bytes[i] & mask;
+            chars[i] = alphabet[index];
+        }
+
+        return new string(chars);
+    }
+}
+
+/*  .    _    +     .  ______   .          .
+ (      /|\      .    |      \      .   +
+     . |||||     _    | |   | | ||         .
+.      |||||    | |  _| | | | |_||    .
+   /\  ||||| .  | | |   | |      |       .
+__||||_|||||____| |_|_____________\__________
+. |||| |||||  /\   _____      _____  .   .
+  |||| ||||| ||||   .   .  .         ________
+ . \|`-'|||| ||||    __________       .    .
+    \__ |||| ||||      .          .     .
+ __    ||||`-'|||  .       .    __________
+.    . |||| ___/  ___________             .
+   . _ ||||| . _               .   _________
+_   ___|||||__  _ \\--//    .          _
+     _ `---'    .)=\oo|=(.   _   .   .    .
+_  ^      .  -    . \.|                    */
